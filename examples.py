89ef7e974c13b358971bd74e96301cf695f0ea4e
Robert F. DeJaco <dejac001@umn.edu>
2019-07-22 22:31:59 -0500

    Remove old examples

--- examples.py
+++ examples.py
@@ -7,26 +7,6 @@ call the functions in this file
 from analysis_factory import RunAllIndepSims, average
 
 
-def butane(T):
-    """
-    analyze the data for butane at T K, located in the example_data/butane_XXXK directory
-        where XXX is the temperature
-    """
-    results = RunAllIndepSims(temperature=T, n_bins=100)
-    indep_sim_list = list(map(str, range(1, 9)))
-    for sim in indep_sim_list:
-        results.run_one_indep_sim('example_data/butane_%iK/%s/' % (T, sim), plot='Yes')
-    average('example_data/butane_%iK' % T, indep_sim_list)
-
-
-def butane_400K():
-    return butane(400)
-
-
-def butane_264K():
-    return butane(264)
-
-
 def pentanal(list_of_indep_sims, T=0, number_of_bins=0, average_fmt=''):
     """
     analyze the data for pentanal at a given temperature=T, located in the example_data/pentanal_XXXK directory
@@ -48,22 +28,8 @@ def pentanal(list_of_indep_sims, T=0, number_of_bins=0, average_fmt=''):
     average('example_data/pentanal_%iK' % T, list_of_indep_sims, fmt=average_fmt)
 
 
-def pentanal_360K_short(**kwargs):
-    indep_sims = list('par'+i for i in  map(str, range(1, 9)))
-    pentanal(indep_sims, T=360, **kwargs)
-
-
 def pentanal_long(temperature, number_of_bins=50, average_fmt='-long'):
     indep_sims = ['par%s' % i for i in map(str, range(1, 17))]
     pentanal(indep_sims, **{'T': temperature, 'number_of_bins': number_of_bins,
                             'average_fmt': '-long'})
-
-
-def example_bin_width():
-    """
-    analyze effect of number of bins chosen on
-        the data for pentanal at 360K, located in the example_data/pentanal_360K directory
-    """
-    for n_bins in [20, 40, 100]:
-        pentanal_360K_short(number_of_bins=n_bins, average_fmt='-%i-bins' % n_bins)
 
